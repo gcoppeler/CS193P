9c6c9f5aadfc3d8c0f064a6d772592d098afaafb
Gary A. Coppeler <coppeler@gmail.com>
2012-07-04 16:43:35 -0400

    Initial commit

---
+++ Calculator/Calculator/CalculatorViewController.h
@@ -0,0 +1,15 @@
+//
+//  CalculatorViewController.h
+//  Calculator
+//
+//  Created by Gary Coppeler on 7/4/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CalculatorViewController : UIViewController
+
+@property (weak, nonatomic) IBOutlet UILabel *display;
+
+@end
